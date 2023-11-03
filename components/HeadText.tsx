6ab14397215c15b3994b3a150233feb3d5cf5d93
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-11-03 14:42:25 +0530

    hero section changes

---
+++ components/HeadText.tsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion";
+
+const TextAnimation = () => {
+  const text1 = "YOUR PDF";
+  const text2 = "YOUR WAY";
+
+  return (
+    <div className="text-center pt-14 md:pt-28">
+      <h1 className="font-extrabold text-5xl sm:text-7xl">
+        {text1.split("").map((char, index) => (
+          <motion.span
+            key={index}
+            initial={{ opacity: 0, x: 400 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: index * 0.1, delay: index * 0.1 }}
+          >
+            {char}
+          </motion.span>
+        ))}
+        <br />
+        {text2.split("").map((char, index) => (
+          <motion.span
+            key={index}
+            initial={{ opacity: 0, x: 400 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: index * 0.2, delay: index * 0.2 }}
+          >
+            {char}
+          </motion.span>
+        ))}
+      </h1>
+    </div>
+  );
+};
+
+export default TextAnimation;
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,26 +1,77 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
 import { Button } from "@/components/ui/button";
-import { ArrowRightCircle } from "lucide-react";
-import Link from "next/link";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import React from "react";
+import { motion as m } from "framer-motion";
+import { ArrowUpRightSquare, GithubIcon, LinkedinIcon } from "lucide-react";
+import Link from "next/link";
+import TextAnimation from "@/components/HeadText";
 
-const Hero = () => {
+export default function Hero() {
   return (
-    <section className="min-h-screen max-w-full bg-transparent text-slate-200 flex flex-col gap-3 items-center   pt-24  relative">
-      <div className="absolute -z-10 brightness-50  inset-0 bg-[url('/assets/hero1.jpg')] bg-cover bg-center" />
-      <h1 className="text-3xl  sm:text-5xl md:text-7xl mt-16 sm:mt-36 text-center font-bold">
-        Your PDFs, Your Way
-      </h1>
-      <h3 className=" text-sm sm:text-lg md:text-xl text-center">
-        Effortlessly Edit, Store, and Enhance Your PDFs.
-      </h3>
-      <Button className="text-black mt-4" asChild>
-        <Link href="/signup">
-          Start a new journey
-          <ArrowRightCircle className="ml-2" />
-        </Link>
-      </Button>
+    <section
+      id="hero"
+      className="z-0 pb-24 relative max-w-full w-full   min-h-screen px-6"
+    >
+      <div>
+        <TextAnimation />
+        <p className="text-center mt-4 font-normal">
+          Simplify Your Document Management
+          <br />
+          Store, Edit, and Access Your PDFs from Anywhere, Anytime
+        </p>
+        <Card className="max-w-[180px] mx-auto mt-6 p-2 bg-gray-200 dark:bg-slate-700">
+          <CardHeader className="p-0">
+            <div className="w-full">
+              <Button className="w-full block " variant="default">
+                <Link href="/signup">Try It Now</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent className=" p-0 text-sm text-center mt-2">
+            Free For Personal Use
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className=" flex flex-col sm:flex-row sm:flex-wrap  sm:justify-center lg:justify-around  min-h-[40vh] h-full    mt-8 max-w-full w-full px-8 gap-2">
+        <Card className="min-w-[14rem]  sm:w-max self-end   w-full text-center bg-green-200 dark:bg-slate-700">
+          <CardHeader className="text-xl font-bold">
+            Your Pdf
+            <br />
+            On Secured
+            <br />
+            Hands
+          </CardHeader>
+        </Card>
+        <Card className="min-w-[16rem]  sm:w-max min-h-[40vh]  h-full grayscale  bg-[url('/assets/h1.jpg')]  w-full bg-cover bg-center " />
+        <Card className="min-w-[14rem]  sm:w-max self-center   w-full text-center bg-green-300 dark:bg-slate-700">
+          <CardHeader className="text-xl font-bold">
+            Professional
+            <br />
+            Way To
+            <br />
+            Edit Pdf's
+          </CardHeader>
+        </Card>
+        <Card className="min-w-[14rem]  sm:w-max  bg-[url('/assets/cloud.jpg')] w-full bg-cover bg-center overflow-hidden ">
+          <CardHeader className="backdrop-brightness-75 text-2xl h-full text-slate-100 font-bold">
+            Access Your Files
+            <br />
+            On The Go!
+          </CardHeader>
+        </Card>
+        <Card className="min-w-[14rem]  sm:w-max self-start   w-full text-center bg-green-400 dark:bg-slate-700">
+          <CardHeader className="text-xl font-bold">
+            Customize PDFs
+            <br />
+            Seamlessly
+            <br />
+            Let's Start!
+          </CardHeader>
+        </Card>
+      </div>
     </section>
   );
-};
-
-export default Hero;
+}
